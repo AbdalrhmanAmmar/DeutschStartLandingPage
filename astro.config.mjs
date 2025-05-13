14ac9cde43dce785bdaf8a5ca524bfbc0284325f
AbdalrhmanAmmar <bodyty2n@gmail.com>
2025-05-14 02:46:43 +0300

    fix-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.deutschstart.com',
-  integrations: [
-    sitemap({
-      filter: (page) => !page.includes('404'),
-      customPages: [
-        'https://www.deutschstart.com/',
-        'https://www.deutschstart.com/about/',
-        'https://www.deutschstart.com/faq/'
-      ]
-    })
-  ]
+    site: 'https://www.deutschstart.com',
+    integrations: [
+        sitemap({
+            lastmod: new Date(),
+            filter: (page) => page !== '/404/',
+            customPages: [
+                'https://www.deutschstart.com/',
+                'https://www.deutschstart.com/about/',
+                'https://www.deutschstart.com/faq/'
+            ],
+            changefreq: 'weekly',
+            priority: 1
+        })
+    ]
 });
